121cfb4aea90fa2fb4fc366bc74ccdbfd05c37be
prasad sripathi <prasad18218@gmail.com>
2026-01-27 07:50:25 +0530

    terraform

---
+++ locals.tf
@@ -0,0 +1,22 @@
+locals {
+    common_name_suffix = "${var.project_name}-${var.environment}"
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids = split (",", data.aws_ssm_parameter.private_subnet_ids.value)
+    ami_id = data.aws_ami.terraform.id
+    sg_id = data.aws_ssm_parameter.sg_id
+    vpc_id = data.aws_ssm_parameter.vpc_id
+    tg_part = "${var.component}" == "frontend" ? 80 : 8080
+    health_check_path = "${var.component}" == "frontend" ? "/" : "/health"
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
+    frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
+   
+    listener_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
+   
+    host_context = "${var.component}" == "frontend" ? "${var.project_name}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
+    
+    common_tags = {
+        Project = var.project_name
+        Environment = var.environment
+        Terraform = "true"
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+    default = "roboshop"
+  
+}
+
+variable "environment" {
+
+    default = "dev"
+  
+}
+
+variable "component" {
+
+  
+}
+
+variable "rule_priority" {
+  
+}
+
+variable "domain_name" {
+    default = "spdevhops.shop"
+  
+}
