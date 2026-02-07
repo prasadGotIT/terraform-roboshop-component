e5cb117a9401bc6394590cbc7062a8c605ec34e0
prasad sripathi <prasad18218@gmail.com>
2026-02-07 08:41:40 +0530

    terraform module

--- data.tf
+++ data.tf
@@ -19,9 +19,15 @@ data "aws_ami" "terraform" {
 }
 
 data "aws_ssm_parameter" "sg_id" {
-    name = "/${var.project_name}/${var.environment}/${component}_sg_id"
+    name = "/${var.project_name}/${var.environment}/${var.component}_sg_id"
   
 }
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+    name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+  
+}
+
 
 data "aws_ssm_parameter" "vpc_id" {
     name = "/${var.project_name}/${var.environment}/vpc_id"
